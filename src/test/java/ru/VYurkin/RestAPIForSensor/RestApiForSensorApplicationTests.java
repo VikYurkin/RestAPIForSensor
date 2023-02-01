a818420811f77c9bf5fdbf84586b4f8a84b3c1fe
Viktor Yurkin <viktor-jurkin@rambler.ru>
2023-02-02 02:05:28 +0600

    Day1 realisation Get and Post method

---
+++ src/test/java/ru/VYurkin/RestAPIForSensor/RestApiForSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.VYurkin.RestAPIForSensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiForSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
